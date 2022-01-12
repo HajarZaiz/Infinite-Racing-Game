d270fa7867b38cc0a7a5578beb849874952ba06f
HajarZaiz <55946014+HajarZaiz@users.noreply.github.com>
2022-01-12 16:00:07 +0300

    Semi-Random Obstacle Generation

--- Assets/Scripts/Infinite Ground/InfiniteGround.cs
+++ Assets/Scripts/Infinite Ground/InfiniteGround.cs
@@ -9,13 +9,21 @@ public class InfiniteGround : MonoBehaviour
     [SerializeField] private Vector3 spawnNext;
 
     //Obstacles Spawning Variable
-    //Plane dimensions
-    [SerializeField] private float plane_x;
-    [SerializeField] private float plane_z;
+    //----------------------Number of cells per row----------------------------
+    [SerializeField] [Range(5, 20)] private int frequency;
+    //----------------------Obstacles Height interval----------------------------
+    [SerializeField] private int heightMin;
+    [SerializeField] private int heightMax;
+    //----------------------Number of obstacles per cell----------------------------
+    [SerializeField] private float density;
     [SerializeField] private GameObject obstaclePrefab;
     //Corners of plane
-    Vector3[] vertices;
+    private Vector3[] vertices;
     private int i = 0;
+    private float z_pos;
+
+    //Obstacles destruction variables
+    [SerializeField] private Transform ship;
 
 
     // Start is called before the first frame update
@@ -25,12 +33,26 @@ public class InfiniteGround : MonoBehaviour
         SpawnGround();
     }
 
+    void Update()
+    {
+        //Continuously destroy obstacles behind
+        GameObject[] obstacles = GameObject.FindGameObjectsWithTag("Obstacle");
+        foreach (GameObject o in obstacles)
+            if(ship.position.z > o.transform.position.z + 10)
+                GameObject.Destroy(o);
+    }
+
     public void SpawnGround()
     {
         //Spawn the plane at the next position
         GameObject g = Instantiate(ground, spawnNext, Quaternion.identity);
         spawnNext = g.transform.GetChild(1).transform.position;
-        
+
+        SpawnObstacles();
+    }
+
+    public void SpawnObstacles()
+    {
         //Global coordinates of plane corners
         Vector3 topLeft = new Vector3(-100, 0, 200 + i);
         Vector3 topRight = new Vector3(100, 0, 200 + i);
@@ -38,19 +60,49 @@ public class InfiniteGround : MonoBehaviour
         Vector3 bottomRight = new Vector3(100, 0, i);
         i = i + 200;
 
-        for (var j = 0; j < 1000; j++)
+        //Chunks iteration variables
+        float x_increment1 = 0;
+        float x_increment2 = 200 / frequency;
+
+        float z_increment1 = 0;
+        float z_increment2 = 200 / frequency;
+        var cellNumber = frequency * frequency;
+
+        //Split the terrain to chunks (Initially split to frequency*frequency chunks that increase size with difficulty)
+        for(var j = 1; j < cellNumber; j++)
         {
-            //Randomize Spawning position
-            var x_rand = Random.Range(topLeft.x, topRight.x);
-            var z_rand = Random.Range(bottomLeft.z, topLeft.z);
+            //Number of obstacles per cell
+            for (var k = 0; k < density; k++)
+            {
+                //Randomize Spawning position in that chunk
+                var x_rand = Random.Range(topLeft.x + x_increment1, topLeft.x + x_increment2);
+                var z_rand = Random.Range(bottomLeft.z + z_increment1, bottomLeft.z + z_increment2);
 
-            //Randomize Y scale
+                //Randomize Y scale
+                var y_rand = Random.Range(heightMin, heightMax);
 
+                //Spawn Obstacle
+                GameObject obs = Instantiate(obstaclePrefab, new Vector3(x_rand, y_rand / 2, z_rand), Quaternion.identity);
+                obs.transform.localScale = new Vector3(9, y_rand, 1);
+            }
 
-            //Spawn Obstacle
-            GameObject obs = Instantiate(obstaclePrefab, new Vector3(x_rand, 0.5f, z_rand), Quaternion.identity);
+            //Update the chunk cell
+            x_increment1 = x_increment1 + (200 / frequency);
+            x_increment2 = x_increment2 + (200 / frequency);
+
+            //Move to the next row if needed
+            if (j % frequency == 0)
+            {
+                z_increment1 = z_increment1 + (200 / frequency);
+                z_increment2 = z_increment2 + (200 / frequency);
+
+                //Reset the chunk cell
+                x_increment1 = 0;
+                x_increment2 = 200 / frequency;
+            }
+
         }
-
     }
+
 
 }
