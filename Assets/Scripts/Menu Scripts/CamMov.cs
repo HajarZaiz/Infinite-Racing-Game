1dc8c79f599c26efa3af1b7b5e9f4abee141beb5
HajarZaiz <55946014+HajarZaiz@users.noreply.github.com>
2022-02-02 14:41:53 +0300

    Spawn Particle System

--- Assets/Scripts/Menu Scripts/CamMov.cs
+++ Assets/Scripts/Menu Scripts/CamMov.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class CamMov : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform t;
+    [SerializeField] private GameObject panel1;
+    [SerializeField] private GameObject panel2;
+    private bool pressed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,18 @@ public class CamMov : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.AddForce(new Vector3(0f, 0f, -1f), ForceMode.Force);
+        if (Input.anyKeyDown)
+        {
+            pressed = true;
+        }
+        if (transform.position.z > -9 && pressed)
+        {
+            panel1.SetActive(false);
+            rb.AddForce(new Vector3(0f, 0f, -1f), ForceMode.Force);
+        }
+        else
+        {
+            rb.Sleep();
+        }
     }
 }
