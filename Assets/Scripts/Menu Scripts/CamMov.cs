d4154d220ba3fad2aae46681eb1447053c31c091
HajarZaiz <55946014+HajarZaiz@users.noreply.github.com>
2022-02-03 23:27:34 +0300

    Menu Spawn and display

--- Assets/Scripts/Menu Scripts/CamMov.cs
+++ Assets/Scripts/Menu Scripts/CamMov.cs
@@ -7,12 +7,22 @@ public class CamMov : MonoBehaviour
     [SerializeField] private Rigidbody rb;
     [SerializeField] private Transform t;
     [SerializeField] private GameObject panel1;
+    [SerializeField] private GameObject spawnRing;
     [SerializeField] private GameObject panel2;
+    [SerializeField] private GameObject menuCanvas;
+    [SerializeField] private GameObject ship;
     private bool pressed;
+    private bool spawn;
+    private Vector3 initialScale; 
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnRing.SetActive(false);
+        spawn = false;
+        menuCanvas.SetActive(false);
+        initialScale = new Vector3(1f, 1f, 1f);
+        ship.transform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
@@ -27,9 +37,38 @@ public class CamMov : MonoBehaviour
             panel1.SetActive(false);
             rb.AddForce(new Vector3(0f, 0f, -1f), ForceMode.Force);
         }
-        else
+        else if(transform.position.z <= -9)
         {
             rb.Sleep();
+            if (!spawn)
+            {
+                spawn = true;
+                StartCoroutine(SpawnRingCoroutine());
+            }
         }
     }
+
+    private IEnumerator SpawnRingCoroutine()
+    {
+        yield return new WaitForSeconds(3);
+        spawnRing.SetActive(true);
+        StartCoroutine(ScaleUpShip(5f));
+        menuCanvas.SetActive(true);
+    }
+
+    private IEnumerator ScaleUpShip(float time)
+    {
+        float i = 0;
+        float rate = 1 / time;
+
+        Vector3 scaleFrom = Vector3.zero;
+        Vector3 scaleTo = initialScale;
+        while(i < 1)
+        {
+            i += Time.deltaTime * rate;
+            ship.transform.localScale = Vector3.Lerp(scaleFrom, scaleTo, i);
+            yield return 0;
+        }
+    }
+
 }
